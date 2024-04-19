49298d4c52399270967f644b64bcd583313dbfda
ryu2kk <ryu799@live.com.pt>
2024-04-19 14:34:55 +0100

    Criado a : 19/04/2024

---
+++ BoardGameHub.Domain/Entities/Jogos.cs
@@ -0,0 +1,14 @@
+﻿namespace BoardGameHub.Domain.Entities
+{
+    public class Jogos
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
+        public string? Description { get; set; }
+        public int PlayerNumber { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Additional { get; set; }
+
+    }
+}
