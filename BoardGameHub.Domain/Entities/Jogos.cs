3e0cdd33e820526ced8df9914f80b7664f5841f3
ryu2kk <ryu799@live.com.pt>
2024-05-03 16:53:14 +0100

    03/05/2024

--- BoardGameHub.Domain/Entities/Jogos.cs
+++ BoardGameHub.Domain/Entities/Jogos.cs
@@ -1,14 +1,33 @@
-﻿namespace BoardGameHub.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace BoardGameHub.Domain.Entities
 {
     public class Jogos
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+
+		[MaxLength(50)]
         public required string Name { get; set; }
-        public required string Category { get; set; }
-        public string? Description { get; set; }
-        public int PlayerNumber { get; set; }
-        public string? ImageUrl { get; set; }
-        public string? Additional { get; set; }
+
+		[MaxLength(50)]
+		[Display(Name = "Categoria")]
+		public required string Category { get; set; }
+
+		[Display(Name = "Descrição")]
+		public string? Description { get; set; }
+
+		[Display(Name = "Número de Jogadores")]
+		[Range(1,20)]
+		public int PlayerNumber { get; set; }
+
+		[Display(Name = "Imagem")]
+		public string? ImageUrl { get; set; }
+
+		[Display(Name = "Adicional")]
+		public string? Additional { get; set; }
 
     }
 }
--- BoardGameHub.Web/Controllers/JogosController.cs
+++ BoardGameHub.Web/Controllers/JogosController.cs
@@ -1,22 +1,116 @@
-﻿using BoardGameHub.Infrastructure.Data;
+﻿using BoardGameHub.Domain.Entities;
+using BoardGameHub.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoardGameHub.Web.Controllers
 {
-    public class JogosController : Controller
-    {
-        private readonly ApplicationDbContext _db;
+	public class JogosController : Controller
+	{
+		private readonly ApplicationDbContext _db;
 
-        public JogosController(ApplicationDbContext db)
-        {
-            _db= db;
-        }
+		public JogosController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
 
-        public IActionResult Index()
-        {
-            var jogos= _db.db_Jogos.ToList();
+		public IActionResult Index()
+		{
+			var jogos = _db.db_Jogos.ToList();
 
-            return View(jogos);
-        }
-    }
+			return View(jogos);
+		}
+
+		//GET
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Jogos obj)
+		{
+			if (obj.Name == obj.Description) 
+			{
+				ModelState.AddModelError("Description", "A descrição não pode ser igual ao nome");		
+			}
+
+			if (ModelState.IsValid)
+			{
+				_db.db_Jogos.Add(obj);
+				_db.SaveChanges();
+
+                TempData["success"] = "O jogo foi criado com sucesso.";
+
+                return RedirectToAction(nameof(Index));
+			}
+
+            TempData["error"] = "Erro ao criar o Jogo.";
+            return View(obj);
+		}
+
+		public IActionResult Update(int jogosId) 
+		{
+			Jogos? obj = _db.db_Jogos.FirstOrDefault(u => u.Id == jogosId);
+
+
+			if (obj == null)
+			{
+				return RedirectToAction("Error", "Home");
+			}
+
+			return View(obj);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Update(Jogos obj)
+		{
+			if (ModelState.IsValid && obj.Id > 0)
+			{
+				_db.db_Jogos.Update(obj);
+				_db.SaveChanges();
+
+                TempData["success"] = "O jogo foi editado com sucesso.";
+
+                return RedirectToAction(nameof(Index));
+			}
+
+            TempData["error"] = "Erro ao editar o Jogo.";
+            return View(obj);
+		}
+		public IActionResult Delete(int jogosId)
+		{
+			Jogos? obj = _db.db_Jogos.FirstOrDefault(u => u.Id == jogosId);
+
+
+			if (obj == null)
+			{
+				return RedirectToAction("Error", "Home");
+			}
+
+			return View(obj);
+		}
+
+		[HttpPost]
+		public IActionResult Delete(Jogos obj)
+
+		{
+			Jogos? objFromDb = _db.db_Jogos.FirstOrDefault(u => u.Id == obj.Id);
+
+			if (objFromDb is not null)
+			{
+				_db.db_Jogos.Remove(objFromDb);
+				_db.SaveChanges();
+
+				TempData["success"] = "O jogo foi apagado com sucesso.";
+
+				return RedirectToAction(nameof(Index));
+			}
+
+            TempData["error"] = "Erro ao apagar o Jogo.";
+            return View(obj);
+		}
+	}
 }
